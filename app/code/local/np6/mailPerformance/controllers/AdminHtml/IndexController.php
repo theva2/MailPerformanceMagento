411a7d848bf9e086974cbeaeaa50517376fa8395
woltek <schmitterarnaud@gmail.com>
2015-05-28 15:39:25 +0100

    étape 6 bis

--- app/code/local/np6/mailPerformance/controllers/AdminHtml/IndexController.php
+++ app/code/local/np6/mailPerformance/controllers/AdminHtml/IndexController.php
@@ -7,4 +7,25 @@ class np6_mailPerformance_Adminhtml_IndexController extends Mage_Adminhtml_Contr
           $this->loadLayout();
           $this->renderLayout();
   }
+
+  public function saveAction()
+	{
+	    //on recuperes les données envoyées en POST
+	    $nom = ''.$this->getRequest()->getPost('nom');
+	    $prenom = ''.$this->getRequest()->getPost('prenom');
+	    $telephone = ''.$this->getRequest()->getPost('telephone');
+	    //on verifie que les champs ne sont pas vide
+	    if(isset($nom)&&($nom!='') && isset($prenom)&&($prenom!='')
+	                               && isset($telephone)&&($telephone!='') )
+	   {
+	      //on cree notre objet et on l'enregistre en base
+	      $contact = Mage::getModel('mailPerformance/mailPerformance');
+	      $contact->setData('nom', $nom);
+	      $contact->setData('prenom', $prenom);
+	      $contact->setData('telephone', $telephone);
+	      $contact->save();
+	   }
+	   //on redirige l’utilisateur vers la méthode index du controller indexController
+	   $this->_redirect('adminMP/adminhtml_index');
+	}
 }
